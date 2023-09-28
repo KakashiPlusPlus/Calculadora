82467b845b839c984d590e857c4419c642efead4
KakashiPlusPlus <alvaro.mortellado@gmail.com>
2023-09-27 23:58:17 -0300

    First commit. I create the project in java and some files with some code with a window in JFrame.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+public class Calculadora {
+
+    
+    public static void main(String[] args) {
+        Ventana ventana1 = new Ventana();
+        
+    }
+    
+}
---
+++ Calculadora/src/calculadora/Ventana.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+import javax.swing.JFrame;
+
+class Ventana extends JFrame{
+    public Ventana(){
+        this.setVisible(true);
+        this.setSize(400, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        iniciarComponentes();
+    }
+    
+    private void iniciarComponentes(){
+        
+    }
+    
+    
+}
